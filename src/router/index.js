f19ca204918a20ffec314ffbed7245d73b76a32e
Ademola Adedoyin <adedoyin@flutterwavego.com>
2022-01-18 07:30:59 +0100

    removed the default about page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-// import Home from "../views/Home.vue";
 import login from "../views/Login.vue";
 import chat from "../views/Chat.vue";
 import csChat from "../views/Cs-Chat.vue";
@@ -35,15 +34,6 @@ const routes = [
         next({ name: "login" });
       }
     },
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
 ];
 
